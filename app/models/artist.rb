a5f9e4f75b9998af8a843c5f88cca97508961656
Chris Gullickson <cgullickson5@gmail.com>
2018-02-12 20:37:27 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,13 @@
 class Artist < ActiveRecord::Base
   has_many :songs
 
-  def artist_name(@artist)
-
+  def artist_name
+    self.artist.name
   end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+    
 end
